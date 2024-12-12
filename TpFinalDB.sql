dc9e92e7086029f764a3d6e185d52edf48ce4cfa
camilatarifa <142285859+camilatarifa@users.noreply.github.com>
2024-12-12 13:14:39 -0300

    TRABAJO FINAL DE BASE DE DATOS

---
+++ TpFinalDB.sql
@@ -0,0 +1,31 @@
+create database final;
+drop database final;
+use final;
+-- Ejercicio 6
+CREATE TABLE fabricantes (
+    id_fabricante INT PRIMARY KEY,
+    nombre_fabricante VARCHAR(255) NOT NULL
+);
+
+INSERT INTO fabricantes (id_fabricante, nombre_fabricante)
+VALUES(1, 'Fabricante A'),(2, 'Fabricante B'),(3, 'Fabricante C');
+
+CREATE TABLE productos (
+    id_producto INT PRIMARY KEY,
+    id_fabricante INT,
+    nombre_producto VARCHAR(255) NOT NULL,
+    fecha_lanzamiento DATE,
+    FOREIGN KEY (id_fabricante) REFERENCES fabricantes(id_fabricante)
+);
+
+INSERT INTO productos (id_producto, id_fabricante, nombre_producto, fecha_lanzamiento)
+VALUES(1, 1, 'Producto X', '2020-01-01'),(2, 2, 'Producto Y', '2019-12-01'), (3, 3, 'Producto Z', '2021-05-15'); 
+
+-- 6 a)Crear un índice compuesto en las columnas id_fabricante y nombre_producto de la tabla productos.
+create index id_fabricante_y_nombre_producto  on productos (id_fabricante, nombre_producto);
+
+-- b) Crear un índice único en la columna id_producto de la tabla productos.
+create index id_producto on productos (id_producto);
+
+-- c) Modificar el índice idx_productos_id_fabricante_nombre para que sea  único en la columna id_fabricante.
+alter index  idx_productos_id_fabricante_nombre on id_fabricante;
